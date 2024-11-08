c36ed95c8b7601130e10411eb869398e54aeed62
Tai Morioka <148292625+Taimorioka@users.noreply.github.com>
2024-11-08 22:51:28 +0000

    working on it

--- src/main/java/com/frcteam3636/frc2024/subsystems/indexer/IndexerIO.kt
+++ src/main/java/com/frcteam3636/frc2024/subsystems/indexer/IndexerIO.kt
@@ -3,6 +3,13 @@ package com.frcteam3636.frc2024.subsystems.indexer
 import edu.wpi.first.math.geometry.Rotation2d
 import org.littletonrobotics.junction.LogTable
 import org.littletonrobotics.junction.inputs.LoggableInputs
+import com.frcteam3636.frc2024.CANSparkFlex
+import com.frcteam3636.frc2024.REVMotorControllerId
+import com.revrobotics.CANSparkLowLevel
+import kotlin.doubleArrayOf
+import edu.wpi.first.wpilibj.util.Color
+import edu.wpi.first.wpilibj.
+
 
 interface IndexerIO{
     class IndexerInputs : LoggableInputs {
@@ -10,14 +17,14 @@ interface IndexerIO{
         var indexerCurrent: Double = 0.0
         var isSpinningBalloon: Boolean = false
         var hasBalloon: Boolean = false
-        var balloonColor = null
+        var balloonColor: Color = Color.kblack
 
         override fun toLog(table: LogTable?) {
             table?.put("Indexer Wheel Velocity", indexerVelocity)
             table?.put("Indexer Wheel Current", indexerCurrent)
             table?.put("Is spinning", isSpinningBalloon)
             table?.put("Has balloon", hasBalloon)
-            table?.put("Balloon Color", balloonColor)
+            table?.put("Balloon Color", doubleArrayOf(balloonColor.red, balloonColor.green, balloonColor.blue))
         }
 
         override fun fromLog(table: LogTable) {
@@ -25,7 +32,8 @@ interface IndexerIO{
             indexerCurrent = table.get("Indexer Wheel Current", indexerCurrent)
             isSpinningBalloon = table.get("Is spinning", isSpinningBalloon)
             hasBalloon = table.get("Has balloon", hasBalloon)
-            balloonColor = table.get("Balloon Color", balloonColor)
+            val balloonColorArray: DoubleArray = table.get("Balloon Color", doubleArrayOf(balloonColor.red, balloonColor.green, balloonColor.blue))
+            balloonColor = Color(balloonColorArray[0], balloonColorArray[1], balloonColorArray[2])
         }
     }
     fun updateInputs(inputs: IndexerInputs)
@@ -41,14 +49,16 @@ class IndexerIOReal: IndexerIO{
             CANSparkLowLevel.MotorType.kBrushless
         )
 
-    private var colorSensor: DigitalInput = DigitalInput(COLOR_SENSOR_PORT)
+    private val colorSensorDistance = NetworkTables.getEntry("/proximity1")
+
+    private val colorSensorColor = NetworkTables.getEntry("/rawcolor1")
 
     override fun updateInputs(inputs: IndexerIO.IndexerInputs) {
         inputs.indexerVelocity = Rotation2d(indexerWheel.encoder.velocity)
         inputs.indexerCurrent = indexerWheel.outputCurrent
         inputs.isSpinningBalloon = colorSensor.get()
-        inputs.hasBalloon = colorSensor.get()
-        inputs.balloonColor = colorSensor.get()
+        inputs.hasBalloon = colorSensorDistance.getValue() > 1000
+        inputs.balloonColor = colorSensorColor.getValue()
     }
 
     override fun setSpinSpeed(speed: Double) {
@@ -56,7 +66,7 @@ class IndexerIOReal: IndexerIO{
     }
 
     internal companion object Constants {
-        const val COLOR_SENSOR_PORT = 0
+        const val COLOR_SENSOR_PORT = i2cPort
         const val RED_BALLOON_COLOR = (255, 0, 0)
         const val BLUE_BALLOON_COLOR = (0, 0, 255)
     }
