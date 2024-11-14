fe53b01c6cd3afe2ffe2a0e6510fe76e653496b7
Gavin Niederman <gavinniederman@gmail.com>
2024-11-13 17:07:33 -0800

    feat: limelight color detection

--- src/main/java/com/frcteam3636/frc2024/subsystems/indexer/Indexer.kt
+++ src/main/java/com/frcteam3636/frc2024/subsystems/indexer/Indexer.kt
@@ -1,26 +1,30 @@
 package com.frcteam3636.frc2024.subsystems.indexer
 
-import com.revrobotics.ColorMatch
-import edu.wpi.first.wpilibj.util.Color
+import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.Commands.startEnd
 import edu.wpi.first.wpilibj2.command.Subsystem
 import org.littletonrobotics.junction.Logger
 
 object Indexer: Subsystem {
     private var io: IndexerIO = IndexerIOReal()
-    private var colorMatcher = ColorMatch().apply {
-        addColorMatch(RED_BALLOON_COLOR)
-        addColorMatch(BLUE_BALLOON_COLOR)
-    }
 
     var inputs = IndexerIO.IndexerInputs()
-    var currentColor: Color? = null
 
     override fun periodic() {
         io.updateInputs(inputs)
         Logger.processInputs("Indexer", inputs)
-        currentColor = colorMatcher.matchClosestColor(inputs.balloonColor).color
+
+        DriverStation.getAlliance().map {
+            if (
+                (inputs.balloonState == BalloonState.Blue && it == DriverStation.Alliance.Blue)
+                || (inputs.balloonState == BalloonState.Red && it == DriverStation.Alliance.Red)
+            ) {
+                progressBalloon()
+            } else {
+                outtakeBalloon()
+            }
+        }
+
     }
 
     fun progressBalloon(): Command =
@@ -34,8 +38,4 @@ object Indexer: Subsystem {
             {io.setSpinSpeed(-0.5)},
             {io.setSpinSpeed(0.0)}
         )
-
-    val RED_BALLOON_COLOR = Color(255, 0, 0)
-    val BLUE_BALLOON_COLOR = Color(0, 0, 255)
-
 }
